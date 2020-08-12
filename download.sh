af6c3f57b9495a7357d0dc70952bf5702ffa7b8a
MountainMan <46047927+MountainMan12@users.noreply.github.com>
2020-08-12 18:29:33 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,6 +2,8 @@
 
 sudo apt install curl
 
+#-------------1. DATA DOWNLOAD -------------------------
+
 # WE WILL KEEP IT CLEAN AND MAKE A NEW FOLDER
 mkdir sars
 cd sars/
@@ -18,7 +20,33 @@ chmod +x datasets  #Give permission to access the datasets module
 
 unzip SARS2-all.zip
 
-#Run phylo.sh for analysis
-bash phylo.sh
+#-------------2. DATA EXTRACTION -------------------------
+
+#Extract batch data
+echo "Extracting batch sequences from the SARS-CoV2 dataset"
+
+python getseq.py
+
+
+#-------------3. ANALYSIS -------------------------
+
+echo "aligning sequences using MAFFT" 
+
+mafft --auto --reorder --maxiterate 1000 sequence.fasta  > seqalign.fasta
+
+# Viewing the alignment on jalview
+#echo "Viewing the alignment"
+
+#jalview seqalign.fasta
+
+# constructing a phylogenetic tree
+echo "constructing a phylogenetic tree using iqtree"
+
+iqtree -s seqalign.fasta -nt AUTO  -m TEST -bb 1000
+
+# opening the constructed tree
+echo "opening the tree file"
+
+figtree *.treefile
 
 
